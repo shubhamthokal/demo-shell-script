4db003e0ec3f14c783d51aaae8c1fb40f05b6e83
Shubham <shubhamthokal5799@gmail.com>
2025-02-14 09:58:50 +0000

    added timepass

---
+++ bike.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+echo " Hello First"
+
+echo "Second hello"
+
+echo "Hello Third "
+
+echo " My name is Four "
+
+echo " Five "
+
+mkdir -p ZERO
+
+echo " Hello SIX "
+
+echo " TOTAL SIX NUMBERS " > ZERO.TXT
+
+
---
+++ hello.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+hero="Rancho"
+Villian="Virus"
+
+echo "Movie ka hero hai $hero"
+echo " movie ka villian hai $Villian"
+
+echo "current log in user is $USER"
+
+
+read -p "racho ka fullname kya tha" fullname
+
+echo "racho ka fullname tha $fullname "
+
+
+# Punjab Apurva Shubham Sumedh
+
+echo "title: $0 "
+echo "First name: $1 "
+echo "second name: $2 "
+echo "third name: $3 "
+
+echo "Hence sab ka naame hai : $@ "
+
+
+
+
+
